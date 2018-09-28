09ca77cfc9b5d864132aebed78fde3aed52c3d2d
Mukesh Singal <mukesh.singal@kony.com>
2018-09-28 08:46:39 +0530

    Singleton Pattern

---
+++ src/SingletonPattern/AnotherSingleton.java
@@ -0,0 +1,21 @@
+package SingletonPattern;
+
+class AnotherSingleton {
+    private static final AnotherSingleton ourInstance = new AnotherSingleton();
+    private int value;
+
+    private AnotherSingleton() {
+    }
+
+    static AnotherSingleton getInstance() {
+        return ourInstance;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ src/SingletonPattern/Main.java
@@ -0,0 +1,24 @@
+package SingletonPattern;
+
+/**
+ * Singleton pattern is creation design pattern that
+ * Let's you ensure that class has only single instance
+ * and provide global access to the instance.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        /* Using Enum : Thread Safe */
+        MySingleton s1 = MySingleton.INSTANCE;
+        MySingleton s2 = MySingleton.INSTANCE;
+        s1.setValue(23);
+        System.out.println(s2.getValue());
+
+        /* Using Private Constructor: Not Threadsafe */
+        AnotherSingleton singleton = AnotherSingleton.getInstance();
+        AnotherSingleton singleton2 = AnotherSingleton.getInstance();
+        singleton.setValue(4);
+        System.out.println(singleton2.getValue());
+    }
+
+}
